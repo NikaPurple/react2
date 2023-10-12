cf90ca2fd18f3fff33e7c1e2b590d70cd88dddb3
Nika <ccnico8@gmail.com>
2023-10-11 19:19:39 -0500

    V03 definido Componente01 y modificado App js

---
+++ src/pagina/C01componente.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const C01componente = () => {
+  return (
+    <div>
+        <h1>COMPONENTE 01</h1>
+      Componente definida con función flecha (rafce)
+    </div>
+  )
+}
+
+export default C01componente
