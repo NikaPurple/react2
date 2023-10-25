70f2f8533c7bf698d37bba981d67be5d1f2c3c2e
nika <ccnico8@gmail.com>
2023-10-24 19:43:46 -0500

    V8 Configuro firebase y appForm añadio registro

--- src/components/AppForm.js
+++ src/components/AppForm.js
@@ -1,12 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { db } from './firebase';
+import { addDoc, collection } from 'firebase/firestore';
 
 const AppForm = () => {
+
+  /////// GUARDAR Y ACTUALIZAR ///////
+  const campoRegistro = {nombre:"",edad:"",genero:""};
+  const [objeto, setObjeto] = useState(campoRegistro);
+  
+  const handleSubmit = (e) => { //manejador del submit
+  e.preventDefault();
+  
+  try {
+    if (db) {
+      addDoc(collection(db, 'persona'), objeto)
+      console.log("se guardo con exito");
+    }
+  } catch (error) {
+    console.error();
+  }
+}
+
   return (
     <div style={{padding:"10px", background:"orange", textAlign:"center"}}>
-      <h1>AppForm.js</h1>
-      <p>Nombre</p>
-      <p>Edad</p>
-      <button>Guardar</button>
+      <form onSubmit={handleSubmit}>
+        <h1>AppForm.js</h1>
+        <input type='text' placeholder='Nombre...'/>
+        <input type='text' placeholder='Nombre...'/>
+        <select>
+          <option value="">Seleccione genero...</option>
+          <option value="F">Femenino</option>
+          <option value="M">Masculino</option>
+        </select>
+        <button>Guardar/Actualizar</button>
+      </form>
     </div>
   )
 }
